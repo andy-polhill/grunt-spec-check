dc1acc17af2014ae3c59159c7563a41025cea321
Andy Polhill <apolhill@gmail.com>
2015-03-11 08:45:16 +0000

    Renamed to spec check

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 /*
- * grunt-missing-spec
- * https://github.com/andypolhill/grunt-missing-spec
+ * grunt-spec-check d
+ * https://github.com/andypolhill/grunt-spec-check
  *
  * Copyright (c) 2015 Andrew Polhill
  * Licensed under the MIT license.
@@ -23,24 +23,25 @@ module.exports = function(grunt) {
       }
     },
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
+    dirs: {
+      all_tested_spec_format: 'test/fixtures/all_tested_spec_format'
     },
 
     // Configuration to be run (and then tested).
-    missingSpec: {
+    specCheck: {
       default_all_tested: {
         options: {
-          testDir: 'test/fixtures/all_tested_spec_format/test'
+          testDir: '<%= dirs.all_tested_spec_format %>/test',
+          baseDir: '<%= dirs.all_tested_spec_format %>/lib'
         },
         files: {
-          src: ['test/fixtures/all_tested_spec_format/lib/**/*.js']
+          src: ['<%= dirs.all_tested_spec_format %>/lib/**/*.js']
         }
       },
       default_missing_test: {
         options: {
-          testDir: 'test/fixtures/missing_test_spec_format/test'
+          testDir: 'test/fixtures/missing_test_spec_format/test',
+          baseDir: 'test/fixtures/missing_test_spec_format/lib'
         },
         files: {
           src: ['test/fixtures/missing_test_spec_format/lib/**/*.js']
@@ -49,7 +50,8 @@ module.exports = function(grunt) {
       default_all_tested_test_format: {
         options: {
           convention: 'Test.js',
-          testDir: 'test/fixtures/all_tested_test_format/test'
+          testDir: 'test/fixtures/all_tested_test_format/test',
+          baseDir: 'test/fixtures/all_tested_test_format/lib'
         },
         files: {
           src: ['test/fixtures/all_tested_test_format/lib/**/*.js']
@@ -58,7 +60,9 @@ module.exports = function(grunt) {
       default_all_tested_sub_extension_format: {
         options: {
           convention: '.spec.js',
-          testDir: 'test/fixtures/all_tested_sub_extension_format/test'
+          testDir: 'test/fixtures/all_tested_sub_extension_format/test',
+          baseDir: 'test/fixtures/all_tested_sub_extension_format/lib',
+
         },
         files: {
           src: ['test/fixtures/all_tested_sub_extension_format/lib/**/*.js']
@@ -67,6 +71,7 @@ module.exports = function(grunt) {
       warn_severity_missing_test: {
         options: {
           testDir: 'test/fixtures/missing_test_spec_format/test',
+          baseDir: 'test/fixtures/missing_test_spec_format/lib',
           severity: 'warn'
         },
         files: {
@@ -75,7 +80,9 @@ module.exports = function(grunt) {
       },
       default_individual_tested_file: {
         options: {
-          testDir: 'test/fixtures/all_tested_spec_format/test'
+          testDir: 'test/fixtures/all_tested_spec_format/test',
+          baseDir: 'test/fixtures/all_tested_spec_format/lib',
+
         },
         files: {
           src: ['test/fixtures/all_tested_spec_format/lib/tested.js']
@@ -83,18 +90,22 @@ module.exports = function(grunt) {
       },
       default_exclude_missing_test: {
         options: {
-          testDir: 'test/fixtures/missing_test_spec_format/test'
+          testDir: 'test/fixtures/missing_test_spec_format/test',
+          baseDir: 'test/fixtures/missing_test_spec_format/lib',
+          severity: 'warn'
         },
-        files: {
-          src: [
-            'test/fixtures/missing_test_spec_format/lib/**/*.js',
-            '!test/fixtures/missing_test_spec_format/lib/another.js',
-          ]
-        }
+        cwd: 'test/fixtures/missing_test_spec_format/lib',
+        expand: true,
+        filter: 'isFile',
+        src: [
+          '**/*.js',
+          '!another.js',
+        ]
       },
       invalid_severity: {
         options: {
           severity: 'log',
+          baseDir: '',
           testDir: 'test/fixtures/missing_test_spec_format/test'
         },
         files: {
@@ -115,12 +126,11 @@ module.exports = function(grunt) {
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'nodeunit']);
+  grunt.registerTask('test', ['test', 'nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
